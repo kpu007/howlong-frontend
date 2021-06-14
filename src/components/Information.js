e77a6e0c566c4300bcea781192a69a0510649371
kpu007 <kpuli007@fiu.edu>
2021-06-14 10:46:07 -0400

    Update README.md

--- src/components/Information.js
+++ src/components/Information.js
@@ -3,7 +3,6 @@ import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-
 const Information = () => {
     return (
         <div className="App" alignItems="center">
